566597bc13dce2bf254f315c3fb1dd0ef4bad81c
UrielCandelas <123997942+UrielCandelas@users.noreply.github.com>
2023-12-10 23:50:52 -0600

    validacion de caracteres especiales

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -1,21 +1,37 @@
 //Se importa zod para hacer un esquema de registro y logueo
 import { z } from "zod";
 //Se crea el esquema de registro
+const noSpecialChars = (value) => /^[a-zA-Z0-9]+$/.test(value);
+
 export const registerSchema = z.object({
-  //Se pide el nombre de usuario y que sea un string
-  name: z.string({
-    required_error: "El nombre de usuario es requerido",
-  }),
-  lastName: z.string({
-    required_error: "El apellido paterno de usuario es requerido",
-  }),
-  secondLastName: z.string({
-    required_error: "El apellido materno de usuario es requerido",
-  }),
-  userName: z.string({
-    required_error: "El ombre de usuario es requerido",
-  }),
-  //Se pide el email que sea un string y email
+  name: z
+    .string({
+      required_error: "El nombre de usuario es requerido",
+    })
+    .refine(noSpecialChars, {
+      message: "El nombre de usuario no puede contener caracteres especiales",
+    }),
+  lastName: z
+    .string({
+      required_error: "El apellido paterno de usuario es requerido",
+    })
+    .refine(noSpecialChars, {
+      message: "El apellido paterno no puede contener caracteres especiales",
+    }),
+  secondLastName: z
+    .string({
+      required_error: "El apellido materno de usuario es requerido",
+    })
+    .refine(noSpecialChars, {
+      message: "El apellido materno no puede contener caracteres especiales",
+    }),
+  userName: z
+    .string({
+      required_error: "El nombre de usuario es requerido",
+    })
+    .refine(noSpecialChars, {
+      message: "El nombre de usuario no puede contener caracteres especiales",
+    }),
   email: z
     .string({
       required_error: "El email es requerido",
@@ -23,7 +39,6 @@ export const registerSchema = z.object({
     .email({
       message: "Email invalido",
     }),
-  //Se pide que la contraseña sea de minimo 8 caracteres y que sea un string
   password: z
     .string({
       required_error: "La contraseña requerida",
